6f80c85290cd30ce6115c71600e6531c0306d73b
Adilb <iskendermurzattillauulu@gmail.com>
2023-03-25 16:38:54 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        method();
+
+    }
+
+    static void method() {
+        String[] massiv = {"Zeyne", "Abiba", "Adilbek"};
+        for (String i : massiv) {
+            System.out.println("I live you" + i);
+        }
+    }
+
+}
